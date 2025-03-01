61a945a87f1e9a4d5f93fdfe558d0cef91beee9c
cwchanap <cwchanap@connect.ust.hk>
2025-02-28 20:46:39 -0800

    upgrade node

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,29 @@
 	import { initializeStores, Toast, Modal } from '@skeletonlabs/skeleton';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-	import { inject } from '@vercel/analytics'
+	import { inject } from '@vercel/analytics';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
 	interface Props {
 		children?: import('svelte').Snippet;
 	}
 
-	let { children }: Props = $props();
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
 	initializeStores();
 	inject();
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
 <Toast />

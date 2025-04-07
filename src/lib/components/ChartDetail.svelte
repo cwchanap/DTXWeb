f1da61e2da673afc78082fa3ad2077e1aca38ca9
cwchanap <cwchanap@connect.ust.hk>
2025-04-06 18:42:39 -0700

    Add editor redirect button

--- src/lib/components/ChartDetail.svelte
+++ src/lib/components/ChartDetail.svelte
@@ -27,7 +27,15 @@
 	const onSave = createEventDispatcher();
 </script>
 
-<div class="flex-grow rounded-lg bg-white p-6 shadow-md">
+<div class="relative flex-grow rounded-lg bg-white p-6 shadow-md">
+	<a
+		href={`/editor/${simfile?.id}`}
+		target="_blank"
+		rel="noopener noreferrer"
+		class="absolute right-2 top-2 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600"
+	>
+		Open in Editor
+	</a>
 	<h1 class="mb-4 text-2xl font-bold">{simfile?.title}</h1>
 	<div class="mt-4 grid grid-cols-8 gap-4">
 		{@render preview?.()}

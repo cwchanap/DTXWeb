e2cae18ecd1f183e64dc87343ed03cb388c97efa
cwchanap <cwchanap@connect.ust.hk>
2025-08-22 15:31:05 -0700

    feat: add editor page tests for loading, navigation, and interaction

--- e2e/constants.ts
+++ e2e/constants.ts
@@ -1,5 +1,7 @@
 export const BASE_URL = 'http://localhost:5173';
 
 export const PAGES = {
-	BLOG: `${BASE_URL}/blog`
+	BLOG: `${BASE_URL}/blog`,
+	EDITOR: `${BASE_URL}/editor`,
+	EDITOR_WITH_SIMFILE: (simfileId: string) => `${BASE_URL}/editor/${simfileId}`
 } as const;

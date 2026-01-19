ae0e638b1e645fa79f123a93b4269d7ef021fa99
cwchanap <cwchanap@connect.ust.hk>
2026-01-18 16:56:42 -0800

    refactor(game): lazy load XA decoder module to improve initial bundle size and startup performance

--- packages/dtx-web/src/routes/(game)/+layout.ts
+++ packages/dtx-web/src/routes/(game)/+layout.ts
@@ -4,10 +4,10 @@ This line of code is to tell sveltekit to not render the page in the server and 
 */
 export const ssr = false;
 
-import init from 'xa_decoder';
-
 export const load = async () => {
-	void init({}).catch((error) => {
-		console.warn('XA decoder init failed:', error);
-	});
+	void import('xa_decoder')
+		.then(({ default: init }) => init({}))
+		.catch((error) => {
+			console.warn('XA decoder init failed:', error);
+		});
 };
